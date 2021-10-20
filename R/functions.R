79d3603aff3bcbff22d918feae3f0402e9f02e58
Ben Anderson <dataknut@icloud.com>
2021-10-20 20:31:07 +0100

    added makeFlexTable

--- R/functions.R
+++ R/functions.R
@@ -26,3 +26,14 @@ getSolent <- function(dt){
               LAD11NM == "Winchester" ]
   return(res)
 }
+
+
+makeFlexTable <- function(df, cap = "caption"){
+  # makes a pretty flextable - see https://cran.r-project.org/web/packages/flextable/index.html
+  ft <- flextable::flextable(df)
+  ft <- colformat_double(ft, digits = 1)
+  ft <- fontsize(ft, size = 9)
+  ft <- fontsize(ft, size = 10, part = "header")
+  ft <- set_caption(ft, caption = cap)
+  return(flextable::autofit(ft))
+}
